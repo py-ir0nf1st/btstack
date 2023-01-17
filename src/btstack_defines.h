993dac483de7cbddc18b18bbe3c7a201c78d01cd
Milanka Ringwald <mila@ringwald.ch>
2023-01-17 15:10:05 +0100

    btstack_define: add GATTSERVICE_SUBEVENT_CSIS_RIS_MATCH event

--- src/btstack_defines.h
+++ src/btstack_defines.h
@@ -4771,6 +4771,14 @@ typedef uint8_t sm_key_t[16];
 */
 #define GATTSERVICE_SUBEVENT_CSIS_REMOTE_SIRK                                     0x66u
 
+/**
+ * @format 121
+ * @param subevent_code
+ * @param csis_cid
+ * @param match
+*/
+#define GATTSERVICE_SUBEVENT_CSIS_RIS_MATCH                                       0x67u
+
 // MAP Meta Event Group
 
 /**
